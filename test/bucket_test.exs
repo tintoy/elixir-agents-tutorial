be30e3dd335700d787920c779084d581bf537ba8
tintoy <tintoy@tintoy.io>
2016-01-08 10:52:55 +1100

    Refactor tests to use shared setup method and test context to inject bucket under test

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,16 +1,19 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "can store and retrieve a value by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "can store and retrieve a value by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
 
-  test "can remove a value by key" do
-    {:ok, bucket} = KV.Bucket.start_link
+  test "can remove a value by key", %{bucket: bucket} do
     assert KV.Bucket.delete(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
